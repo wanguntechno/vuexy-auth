53f84d401879ef00965dbbe7502ef77c56035271
Skylar83 <37374070+samuelhusein@users.noreply.github.com>
2023-06-25 15:31:00 +0700

    Update : Component

--- src/index.js
+++ src/index.js
@@ -1,70 +1,87 @@
 // ** React Imports
-import { Suspense, lazy } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { Suspense, lazy } from "react"
+// import { createRoot } from "react-dom/client"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AuthProvider } from './context/AuthProvider'
+import App from './App'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // ** Redux Imports
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
+// import { store } from "./redux/store"
+// import { Provider } from "react-redux"
 
-// ** ThemeColors Context
+// // ** ThemeColors Context
 
-import { ThemeContext } from "./utility/context/ThemeColors";
+// import { ThemeContext } from "./utility/context/ThemeColors"
 
-// ** ThemeConfig
-import themeConfig from "./configs/themeConfig";
+// // ** ThemeConfig
+// import themeConfig from "./configs/themeConfig"
 
-// ** Toast
-import { Toaster } from "react-hot-toast";
+// // ** Toast
+// import { Toaster } from "react-hot-toast"
 
-// ** Spinner (Splash Screen)
-import Spinner from "./@core/components/spinner/Fallback-spinner";
+// // ** Spinner (Splash Screen)
+// import Spinner from "./@core/components/spinner/Fallback-spinner"
 
-// ** Ripple Button
-import "./@core/components/ripple-button";
+// // ** Ripple Button
+// import "./@core/components/ripple-button"
 
-// ** PrismJS
-import "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
-import "prismjs/components/prism-jsx.min";
+// // ** PrismJS
+// import "prismjs"
+// import "prismjs/themes/prism-tomorrow.css"
+// import "prismjs/components/prism-jsx.min"
 
-// ** React Perfect Scrollbar
-import "react-perfect-scrollbar/dist/css/styles.css";
+// // ** React Perfect Scrollbar
+// import "react-perfect-scrollbar/dist/css/styles.css"
 
-// ** React Hot Toast Styles
-import "@styles/react/libs/react-hot-toasts/react-hot-toasts.scss";
+// // ** React Hot Toast Styles
+// import "@styles/react/libs/react-hot-toasts/react-hot-toasts.scss"
 
-// ** Core styles
-import "./@core/assets/fonts/feather/iconfont.css";
-import "./@core/scss/core.scss";
-import "./assets/scss/style.scss";
+// // ** Core styles
+// import "./@core/assets/fonts/feather/iconfont.css"
+// import "./@core/scss/core.scss"
+// import "./assets/scss/style.scss"
 
-// ** Service Worker
-import * as serviceWorker from "./serviceWorker";
+// // ** Service Worker
+// import * as serviceWorker from "./serviceWorker"
 
-// ** Lazy load app
-const LazyApp = lazy(() => import("./App"));
+// // ** Lazy load app
+// const LazyApp = lazy(() => import("./App"))
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+// const container = document.getElementById("root")
+// const root = createRoot(container)
 
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Suspense fallback={<Spinner />}>
-        <ThemeContext>
-          <LazyApp />
-          <Toaster
-            position={themeConfig.layout.toastPosition}
-            toastOptions={{ className: "react-hot-toast" }}
-          />
-        </ThemeContext>
-      </Suspense>
-    </Provider>
-  </BrowserRouter>
-);
+// root.render(
+//   <BrowserRouter>
+//     <Provider store={store}>
+//       <Suspense fallback={<Spinner />}>
+//         <ThemeContext>
+//           <LazyApp />
+//           <Toaster
+//             position={themeConfig.layout.toastPosition}
+//             toastOptions={{ className: "react-hot-toast" }}
+//           />
+//         </ThemeContext>
+//       </Suspense>
+//     </Provider>
+//   </BrowserRouter>
+// )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,21 +1,21 @@
 // ** Router imports
-import { useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom"
 
 // ** GetRoutes
-import { getRoutes } from "./routes";
+import { getRoutes } from "./routes"
 
 // ** Hooks Imports
-import { useLayout } from "@hooks/useLayout";
+import { useLayout } from "@hooks/useLayout"
 
 const Router = () => {
   // ** Hooks
-  const { layout } = useLayout();
+  const { layout } = useLayout()
 
-  const allRoutes = getRoutes(layout);
+  const allRoutes = getRoutes(layout)
 
-  const routes = useRoutes([...allRoutes]);
+  const routes = useRoutes([...allRoutes])
 
-  return routes;
-};
+  return routes
+}
 
-export default Router;
+export default Router
